3bda142070d51816ba844565e2cfc1133fe2937f
admin <admin@DESKTOP-59I1ACF>
2021-08-28 19:44:14 +0600

    first commit

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using AppoinmentScheduler.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppoinmentScheduler.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        appoinmentEntities1 db = new appoinmentEntities1();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(tbl_admin admin)
+        {
+            admin.name = admin.name;
+            admin.availabletime = admin.availabletime;
+            db.tbl_admin.Add(admin);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult AppointmentList()
+        {
+            return View(db.tbl_appointment.ToList());
+        }
+    }
+}
---
+++ Controllers/EventController.cs
@@ -0,0 +1,67 @@
+﻿using AppoinmentScheduler.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppoinmentScheduler.Controllers
+{
+    public class EventController : Controller
+    {
+        // GET: Event
+        public appoinmentEntities1 db = new appoinmentEntities1();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetEvents()
+        {
+            var events = db.tbl_appointment.ToList();
+            return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+
+        [HttpPost]
+        public JsonResult SaveEvent(tbl_appointment e){
+            var status = false;
+            if(e.EventID > 0)
+            {
+                var v = db.tbl_appointment.Where(x => x.EventID == e.EventID).FirstOrDefault();
+                //update
+                if (v != null)
+                {
+                    v.Subject = e.Subject;
+                    v.Description = e.Description;
+                    v.Start_date = e.Start_date;
+                    v.End_date = e.End_date;
+                    v.IsFullDay = e.IsFullDay;
+                    v.Theme = e.Theme;
+                }
+            }
+            else
+            {// new
+                db.tbl_appointment.Add(e);
+
+            }
+            db.SaveChanges();
+            status = true;
+            return new JsonResult { Data = new { status = status } };
+
+        }
+        [HttpPost]
+        public JsonResult DeleteEvent(int eventID)
+        {
+            var status = false;
+            var v = db.tbl_appointment.Where(x => x.EventID == eventID).FirstOrDefault();
+            if (v != null)
+            {
+                db.tbl_appointment.Remove(v);
+                db.SaveChanges();
+                status = true;
+            }
+            return new JsonResult { Data = new { status = status } };
+            
+        }
+       
+    }
+}
